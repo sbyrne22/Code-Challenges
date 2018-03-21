4106700e0dfdae45d493d5af939167b924176838
Sean Byrne <spickles@Seans-iMac.local>
2018-03-21 17:36:35 -0400

    fixed to ensure the largest prime factor is found

--- javascript/largest_prime_factor.js
+++ javascript/largest_prime_factor.js
@@ -42,16 +42,17 @@ console.log("Hello");
 //
 // largestPrimeFactor(39585);
 
-
+/////////////////
 
 const largestPrimeFactor = (num) => {
   let divis = [];
   let primeArr = [];
   let prime = true;
   let largestPrimeNum = 0;
+  console.log("Entered Number ->", num);
 
-  for (let j = 2; j <= Math.sqrt(num); j++) {
-    // console.log("stuff", divis[i] % j);
+  // Checks if num is prime
+  for (let j = 2; j <= Math.sqrt(num); j ++) {
       if (num % j == 0) {
           prime = false;
           console.log("Prime?", prime, num);
@@ -61,23 +62,29 @@ const largestPrimeFactor = (num) => {
   }
   if (prime == true) {
     console.log("Sorry", num, "is a prime number and its' only factors are 1 and itself");
-  } else {
-    for (let i = 2; i <= Math.sqrt(num); i ++) {
+  } else { // If not prime continues function
+    // Finds all factors of Num
+    for (let i = 2; i < num; i ++) {
       if (num % i == 0) {
         divis.push(i);
-        console.log(divis);
       }
     }
+    console.log("All Factors", divis);
     for (let i = 0; i < divis.length; i ++) {
-      console.log(divis[i]);
-      for (let j = 2; j <= Math.sqrt(divis[i]); j++) {
-        // console.log("stuff", divis[i] % j);
+      console.log("Current Factor being Checked", divis[i], i);
+      // Finds Which factors are prime
+
+      //!!! Why will 2 not go into this loop?
+      for (let j = 2; j <= Math.sqrt(divis[i]); j ++) {
+        console.log("This is J", j);
+        console.log("Checking...", divis[i]);
           if (divis[i] % j == 0) {
               prime = false;
               console.log("Prime?", prime, divis[i]);
-              break;
+              break; // Inside loop ends and outside loop continues
           }
           prime = true;
+          // console.log("peaches");
       }
       if (prime == true) {
         console.log("Prime?", prime, divis[i]);
@@ -92,4 +99,5 @@ const largestPrimeFactor = (num) => {
   }
 }
 
-largestPrimeFactor(100);
+// Why is 2 not being determined as a prime factor?
+largestPrimeFactor(10);
